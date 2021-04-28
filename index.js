bf0fab91c6c5d6eea2ca6f6221c7fc249c2b2128
Fay Vera <fvera.cavalcante@gmail.com>
2021-04-28 16:27:19 -0400

    commit tree 7c86d4bd64d84bbddbc71090d350015eadf2f808

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ft){
   // Write your code here!
+  if (ft < 400){
+    return "This one is on me!"
+  } else if (ft > 2500){
+    return "No can do."
+  } else if (ft > 2000){
+    return "I will gladly take your thirty bucks."
+  } 
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  if (city === "NYC") {
+    return "Ok, sounds good."
+  } else {
+    return "No go."
+  }
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  if (tip === "generous"){
+    return "Thank you so much."
+  } else if (tip === "not as generous"){
+    return "Thank you."
+  } else {
+    return "Bye."
+  }
 }
